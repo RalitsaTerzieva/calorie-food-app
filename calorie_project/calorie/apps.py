89464e30d2fa0b94692b7e509025c1b833e388d7
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-16 14:04:19 +0500

    Add model and migrations

--- calorie_project/calorie/apps.py
+++ calorie_project/calorie/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CalorieConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "calorie"
+    name = "calorie_project.calorie"
--- calorie_project/calorie/models.py
+++ calorie_project/calorie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
