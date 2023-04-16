3086dccc3390256a4949f92d3d3dc1c343f98a92
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-16 15:40:04 +0500

    Add Consume table

--- calorie_project/calorie/admin.py
+++ calorie_project/calorie/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Food
-# Register your models here.
+from .models import Food, Consume
+
+
 admin.site.register(Food)
+admin.site.register(Consume)
--- calorie_project/calorie/models.py
+++ calorie_project/calorie/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length=200)
     carbs = models.FloatField()
@@ -10,3 +10,12 @@ class Food(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Consume(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user
+    
