9d360c134dbb8317f31489ba336872b50f7d780c
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-17 10:46:19 +0500

    Add Boostrap and view logic for selecting and saving an option

--- calorie_project/calorie/views.py
+++ calorie_project/calorie/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Consume
 
 def index(request):
-    foods = Food.objects.all()
+    if request.method == 'POST':
+        consumed_food = request.POST['consumed_food']
+        consume = Food.objects.get(name=consumed_food)
+        user = request.user
+        consume = Consume(user=user,consumed_food=consume)
+        consume.save()
+        foods = Food.objects.all()
+    else:
+        foods = Food.objects.all()
     return render(request, 'food/index.html', {'foods': foods})
 
